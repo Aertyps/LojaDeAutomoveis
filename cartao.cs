0723e14f099f3de4ea951d13efefdc91302a6986
douglasrosseto <douglascorreia@ucl.br>
2020-05-15 02:56:26 +0000

    cartao e dinheiro  .cs adicionados

---
+++ cartao.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+class Cartao:Pagamento
+{
+  private string tipoCartao;
+  private int parcelas;
+  private string numCartao;
+
+  public Cartao(Pagamento pgto string tpcartao, int parc, string ncart)
+  {
+    Pagamento pgto = new Pagamento();
+    pagamento = pgto;
+    SetTipoCartao(tpcartao);
+    SetParcelas(parc);
+    SetNumCartao(ncart);
+  }
+  public Cartao()
+  {
+
+  }
+
+  public string GetTipoCartao()
+  {
+    return tipoCartao;
+  }
+
+  public void SetTipoCartao(string tp)
+  {
+    tipoCartao = tp;
+  }
+
+  public int GetParcelas()
+  {
+    return parcelas;
+  }
+
+  public void SetParcelas(int p)
+  {
+    int parc = p;
+    if(parc > 0)
+    {
+      parcelas=parc;
+    }
+    else
+    {
+      Console.WriteLine("numero de parcelas inválidas!")
+    }
+  }
+
+  public string GetNumCartao()
+  {
+    return numCartao;
+  }
+
+  public void SetNumCartao(string numc)
+  {
+    string numero = numc;
+    if(numero>0 & numero.Length = 16)
+    {
+      numCartao = numero;
+    }
+    else
+    {
+      Console.WriteLine("numero de cartão inválido, obs.: 16 numeros")
+    }
+  }
+
+}
